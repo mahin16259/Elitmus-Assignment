7d26e95d0d1ea9c5b7afa84a6a250fb4c53d7910
mahin16259 <132412627+mahin16259@users.noreply.github.com>
2023-05-03 05:46:07 +0530

    Add files via upload

--- db/db_config.php
+++ db/db_config.php
@@ -1,1 +1,15 @@
+<?php
+// database configuration
+$host = 'localhost';
+$user = 'root';
+$password = '';
+$database = 'project';
 
+// establish database connection
+$conn = mysqli_connect($host, $user, $password, $database);
+
+// check if connection was successful
+if(!$conn) {
+	die('Database connection error: ' . mysqli_connect_error());
+}
+?>
